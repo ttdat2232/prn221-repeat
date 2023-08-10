c47d5dde180e56de5cd1d7da4083085909afa59c
ttdat2232 <dd220302@gmail.com>
2023-08-10 17:59:34 +0700

    update database: add name column to ClubActivity

---
+++ Repository/Migrations/20230810105823_add_nameColumn_ClubActivityTable.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Repositories.Migrations
+{
+    public partial class add_nameColumn_ClubActivityTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "ClubActivities",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "ClubActivities");
+        }
+    }
+}
